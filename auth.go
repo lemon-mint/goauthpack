16596afa8e02b4e83a1e9f5d35fbbf8ab7e8c93e
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-01-28 21:59:44 +0900

    reduce core

--- auth.go
+++ auth.go
@@ -1,11 +1,41 @@
 package goauthpack
 
-import "golang.org/x/crypto/argon2"
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+	"strconv"
+	"strings"
+
+	"golang.org/x/crypto/argon2"
+)
+
+const recommendedAlgorithm = 0
 
 func hashpass(data, salt []byte, version int) []byte {
 	if version == 0 {
 		//argon2 High
-		return argon2.IDKey(data, salt, 5, 32*1024, 4, 32)
+		return argon2.IDKey(data, salt, 5, 32*1024, 2, 32)
 	}
 	return nil
+}
+
+//structure : {version}${salt}${hash}$...(etc)
+func genAuthString(password string, version int) string {
+	salt := make([]byte, 16)
+	io.ReadFull(rand.Reader, salt)
+	hash := hashpass([]byte(password), salt, version)
+	return strconv.Itoa(version) +
+		"$" + base64.RawURLEncoding.EncodeToString(salt) +
+		"$" + base64.RawURLEncoding.EncodeToString(hash)
+}
+
+func verifyAuthString(AuthString string, password string) (success bool, updateRequired bool, CurrentVersion int) {
+	var version int
+	parts := strings.Split(AuthString, "$")
+	version, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return false, false, 0
+	}
+	return true, true, version
 }
