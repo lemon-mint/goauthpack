b591c2c0acf13e6428363bb5bda82fcbd208e235
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-01-28 17:45:43 +0900

    argon2

--- auth.go
+++ auth.go
@@ -1,1 +1,11 @@
 package goauthpack
+
+import "golang.org/x/crypto/argon2"
+
+func hashpass(data, salt []byte, version int) []byte {
+	if version == 0 {
+		//argon2 High
+		return argon2.IDKey(data, salt, 5, 32*1024, 4, 32)
+	}
+	return nil
+}
